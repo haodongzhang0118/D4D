d6d6cfeca7879e3c2ea8933e459eb1cbc262ffc8
Haodong <haodoz0118@gmail.com>
2024-11-16 14:12:09 -0500

    Revised preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -18,16 +18,16 @@ print("Path to dataset files:", path)
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Preprocess images')
-    parser.add_argument('--train_dir', type=str, help='Directory for train images')
-    parser.add_argument('--val_dir', type=str, help='Directory for valid images')
+    parser.add_argument('--train_dir1', type=str, help='Directory for train images')
+    parser.add_argument('--train_dir2', type=str, help='Directory for valid images')
     parser.add_argument('--tar_dir', type=str, help='Directory for combined images')
     parser.add_argument('--patch_dir', type=str, help='Directory for patched images')
     parser.add_argument('--ps', default=256, type=int, help='Patch size (e.g., 256 for 256x256 patches)')
     parser.add_argument('--num_cores', default=6, type=int, help='Number of CPU cores for parallel processing')
 
     args = parser.parse_args()
-    train_dir = args.train_dir
-    val_dir = args.val_dir
+    train_dir = args.train_dir1
+    val_dir = args.train_dir2
     tar_dir = args.tar_dir
     patch_dir = args.patch_dir
     patch_size = args.ps
